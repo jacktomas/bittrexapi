5c40797f464d6a4c2a7a781400974f071a00cbbf
wenda <1538417087@qq.com>
2017-05-17 17:41:29 +0800

    bittrexapi

--- src/main/scala/com/bitrex/BittrexApi.scala
+++ src/main/scala/com/bitrex/BittrexApi.scala
@@ -7,21 +7,26 @@ package com.bitrex
 
 import com.bittrex.responsebody.GetCurrencies
 import com.fasterxml.jackson.databind.ObjectMapper
-import spray.json.{JsonParser, RootJsonFormat, DefaultJsonProtocol}
 
 import scalaj.http.Http
 
-object BittrexApi extends App{
+object BittrexApi extends App {
 
+  /*
+  * import scalaj.http._
+
+    val response: HttpResponse[String] = Http("http://foo.com/search").param("q","monkeys").asString
+    response.body
+    response.code
+    response.headers
+    response.cookies
+  * */
   private val body: String = Http("https://bittrex.com/api/v1.1/public/getcurrencies").asString.body
 
   val mapper: ObjectMapper = new ObjectMapper
   val getCurrencies = mapper.readValue(body, classOf[GetCurrencies])
 
   println(getCurrencies)
-
-
-
 
 
 }
